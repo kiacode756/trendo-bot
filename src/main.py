b6182df5c751b1ba0975eca5847e866dc8b5aa9b
kiacode756 <bagherikiarash@icloud.com>
2026-07-13 12:58:02 +0330

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,7 @@
-print("Trendo Bot Started...")
+from config import API_ID
+
+print("===================================")
+print(" Trendo Telegram Bot Started ")
+print("===================================")
+
+print("API ID:", API_ID)
