0231b1f58e82144478b89e5c39841f060df4e151
kiacode756 <bagherikiarash@icloud.com>
2026-07-13 12:54:59 +0330

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,1 +1,12 @@
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+PHONE = os.getenv("PHONE")
+
+CHANNEL = os.getenv("CHANNEL")
+
+LOT_SIZE = float(os.getenv("LOT_SIZE", "0.10"))
